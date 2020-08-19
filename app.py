fe7b41ce9f1af6b0789747e4a96fe954e8e3e098
odmishien <tmtm.yagi.007@gmail.com>
2020-08-19 13:12:48 +0900

    orya

--- app.py
+++ app.py
@@ -1,43 +1,19 @@
-import time
-import re
-import os
-import traceback
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-import chromedriver_binary
-import slackweb
+from flask import Flask, jsonify
+from job import get_chrome_driver, get_participants_num
 
-SLACK_WEBHOOK_URL = os.environ.get('SLACK_WEBHOOK_URL')
-GATHER_SPACE_URL = os.environ.get('GATHER_SPACE_URL')
+app = Flask(__name__)
 
-def notify_slack(participants_num):
-    if participants_num == 0:
-        return
-    slack = slackweb.Slack(url=SLACK_WEBHOOK_URL)
-    message = f'*{participants_num}人* がバB419にいるよ'
-    slack.notify(text=message, mrkdwn=True)
-    print(f'send message successfully: {message}')
-
-
-if __name__ == "__main__":
+@app.route("/participants-num", methods=['GET'])
+def participants_num():
     options = Options()
     options.add_argument("--headless")
-    driver = webdriver.Chrome(options=options)
+    driver = get_chrome_driver(options)
+    participants_num = get_participants_num(driver)
+    if participants_num == 0:
+        text = 'バB419には....誰もいません... :crying:'
+    else:
+        text = f'*{participants_num}人* がバB419にいるよ'
+    return jsonify({"text": text})
 
-    try:
-        driver.get(GATHER_SPACE_URL)
-        time.sleep(5)
-        driver.find_element_by_css_selector(
-            "div.horizontal-container.action.test-yes-button").click()
-        time.sleep(10)
-        driver.find_element_by_css_selector("div.GameBody-finish").click()
-        time.sleep(3)
-        participants_text = driver.find_element_by_css_selector(
-            "div.u-header-text").text
-        participants_num = re.search(r'\d+', participants_text).group(0)
-        print(participants_num)
-        notify_slack(int(participants_num) - 1)
-    except:
-        traceback.print_exc()
-    finally:
-        driver.quit()
+if __name__ == '__main__':
+    app.run()
---
+++ job.py
@@ -0,0 +1,46 @@
+import time
+import re
+import os
+import traceback
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import chromedriver_binary
+import slackweb
+
+SLACK_WEBHOOK_URL = os.environ.get('SLACK_WEBHOOK_URL')
+GATHER_SPACE_URL = os.environ.get('GATHER_SPACE_URL')
+
+def notify_slack(participants_num):
+    if participants_num == 0:
+        return
+    slack = slackweb.Slack(url=SLACK_WEBHOOK_URL)
+    message = f'*{participants_num}人* がバB419にいるよ'
+    slack.notify(text=message, mrkdwn=True)
+    print(f'send message successfully: {message}')
+
+def get_chrome_driver(options):
+    return webdriver.Chrome(options=options)
+
+def get_participants_num(driver):
+    driver.get(GATHER_SPACE_URL)
+    time.sleep(5)
+    driver.find_element_by_css_selector(
+        "div.horizontal-container.action.test-yes-button").click()
+    time.sleep(10)
+    driver.find_element_by_css_selector("div.GameBody-finish").click()
+    time.sleep(3)
+    participants_text = driver.find_element_by_css_selector(
+        "div.u-header-text").text
+    participants_num = re.search(r'\d+', participants_text).group(0)
+    return int(participants_num) - 1
+
+if __name__ == "__main__":
+    options = Options()
+    options.add_argument("--headless")
+    driver = get_chrome_driver(options)
+    try:
+        notify_slack(get_participants_num(driver))
+    except:
+        traceback.print_exc()
+    finally:
+        driver.quit()
